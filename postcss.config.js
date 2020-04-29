923ff39836e0d6803f1ffd15da4ced0a7bd4e692
Jules Matsounga <hyoakato@gmail.com>
2020-04-29 14:58:42 +0200

    fix: purge only non used class

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,55 @@
 const tailwindcss = require('tailwindcss');
 
-const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+// const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
-const plugins = [];
-plugins.push(tailwindcss)
-plugins.push(tailwindcss('tailwind.config.js'))
+// const plugins = [];
+// plugins.push(tailwindcss)
+// plugins.push(tailwindcss('tailwind.config.js'))
 
-if (!IS_DEVELOPMENT) {
-    const purgecss = require('@fullhuman/postcss-purgecss');
+// if (!IS_DEVELOPMENT) {
+//     const purgecss = require('@fullhuman/postcss-purgecss');
     
-    class TailwindExtractor {
-        static extract(content) {
-            return content.match(/[A-z0-9-:\/]+/g) || [];
-        }
-    }
+//     // class TailwindExtractor {
+//     //     static extract(content) {
+//     //         return content.match(/[A-z0-9-:\/]+/g) || [];
+//     //     }
+//     // }
     
-    plugins.push(
-        purgecss({
-            content: ['src/*.html'],
-            extractors: [
-                {
-                    extractor: TailwindExtractor,
-                    extensions: ['html']
-                }
-            ],
-        })
-    );
-}
+//     plugins.push(
+//         purgecss({
+//             content: ['src/*.html'],
+//             extractors: [
+//                 {
+//                     extractor: TailwindExtractor,
+//                     extensions: ['html']
+//                 }
+//             ],
+//         })
+//     );
+// }
 
-module.exports = { plugins }
+// module.exports = { plugins }
+
+
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project 
+    content: [
+      'index.html',
+    ],
+  
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  })
+  
+  module.exports = {
+    plugins: [
+      tailwindcss,
+      require('autoprefixer'),
+      tailwindcss('tailwind.config.js'),
+      ...process.env.NODE_ENV === 'production'
+        ? [purgecss]
+        : []
+    ]
+  }
